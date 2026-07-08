d4d54ddf7f2559c884527eb98d075138acfe5b5a
fregante <me@fregante.com>
2026-07-08 17:20:47 +0800

    Componentize options page /4 (#9783)

--- source/options.tsx
+++ source/options.tsx
@@ -2,84 +2,102 @@ import 'webext-base-css/webext-base.css';
 import './options.css';
 import {enableTabToIndent} from 'indent-textarea';
 import {$, $$} from 'select-dom';
-import type {SyncedForm} from 'webext-options-sync-per-domain';
 import 'webext-bugs/target-blank';
+import elementReady from 'element-ready';
+import {assertDefined} from 'ts-extras';
 
 import clearCacheHandler from './helpers/clear-cache-handler.js';
 import {perDomainOptions} from './options-storage.js';
-import initFeatureList, {updateListDom} from './options/feature-list.js';
 import initToggleAllButtons from './options/toggle-all.js';
+
+function moveDisabledFeaturesToTop(): void {
+	const container = $('.js-features');
+	const features = $$('feature-item').toSorted((a, b) => a.dataset.text!.localeCompare(b.dataset.text!));
+	const grouped = Object.groupBy(features, feature => {
+		const checkbox = $('input.feature-checkbox', feature);
+		return checkbox.checked ? 'on' : checkbox.disabled ? 'broken' : 'off';
+	});
+
+	for (const group of [grouped.off, grouped.broken, grouped.on]) {
+		if (group) {
+			for (const feature of group) {
+				container.append(feature);
+			}
+		}
+	}
+}
+
+function updateListDom(): void {
+	moveDisabledFeaturesToTop();
+
+	// Notify <feature-count> that the DOM state has updated
+	globalThis.dispatchEvent(new CustomEvent('rgh:update-count'));
+}
 
 function informComponentOfExternalUpdate(field: HTMLInputElement | HTMLTextAreaElement): void {
 	field.dispatchEvent(new InputEvent('input', {bubbles: true}));
 }
 
-async function generateDom(): Promise<SyncedForm> {
-	await initFeatureList();
+assertDefined(await elementReady('.js-features', {
+	stopOnDomReady: false,
+	signal: AbortSignal.timeout(500),
+}));
 
-	// Update list from saved options
-	const syncedForm = await perDomainOptions.syncForm('form');
+// Update list from saved options
+const syncedForm = await perDomainOptions.syncForm('form');
 
-	// <token-input> runs before the value is set, so it detects `firstRun` to avoid validation on an empty form.
-	// This triggers a proper run
-	for (const tokenField of $$('token-input input')) {
-		informComponentOfExternalUpdate(tokenField);
+// <token-input> runs before the value is set, so it detects `firstRun` to avoid validation on an empty form.
+// This triggers a proper run
+for (const tokenField of $$('token-input input')) {
+	informComponentOfExternalUpdate(tokenField);
+}
+
+// Decorate list
+updateListDom();
+initToggleAllButtons();
+
+// JS loaded, remove message before it appears
+$('#js-failed').remove();
+
+// Update domain-dependent page content when the domain is changed
+syncedForm.onChange(async domain => {
+	const host = domain === 'default' ? 'github.com' : domain;
+
+	// Point the link to the right domain
+	$('a#personal-token-link').host = host;
+
+	for (const element of $$([
+		// Hot fixes are not used on GHE
+		'hot-fixes',
+
+		// There's only one button, it doesn't depend on GHE https://github.com/refined-github/refined-github/issues/7704
+		'action-link',
+	])) {
+		element.toggleAttribute('enterprise', domain !== 'default');
 	}
 
-	// Decorate list
+	for (const element of $$('storage-usage[item]')) {
+		element.setAttribute('item', domain === 'default' ? 'options' : 'options:' + domain);
+	}
+
+	for (const element of $$('token-input')) {
+		element.setAttribute('host', host);
+		informComponentOfExternalUpdate($('input', element));
+	}
+
 	updateListDom();
-	initToggleAllButtons();
+});
 
-	// JS loaded, remove message before it appears
-	$('#js-failed').remove();
+// Refresh page when permissions are changed (because the dropdown selector needs to be regenerated)
+chrome.permissions.onRemoved.addListener(() => {
+	location.reload();
+});
+chrome.permissions.onAdded.addListener(() => {
+	location.reload();
+});
 
-	return syncedForm;
-}
+// Improve textareas editing
+enableTabToIndent('textarea');
 
-function addEventListeners(syncedForm: SyncedForm): void {
-	// Update domain-dependent page content when the domain is changed
-	syncedForm.onChange(async domain => {
-		const host = domain === 'default' ? 'github.com' : domain;
-
-		// Point the link to the right domain
-		$('a#personal-token-link').host = host;
-
-		// Hot fixes are not used on GHE
-		$('hot-fixes').toggleAttribute('enterprise', domain !== 'default');
-
-		// Hide "Button link" on GHE domains https://github.com/refined-github/refined-github/issues/7704
-		$('action-link').toggleAttribute('enterprise', domain !== 'default');
-
-		for (const element of $$('storage-usage[item]')) {
-			element.setAttribute('item', domain === 'default' ? 'options' : 'options:' + domain);
-		}
-
-		for (const element of $$('token-input')) {
-			element.setAttribute('host', host);
-			informComponentOfExternalUpdate($('input', element));
-		}
-
-		updateListDom();
-	});
-
-	// Refresh page when permissions are changed (because the dropdown selector needs to be regenerated)
-	chrome.permissions.onRemoved.addListener(() => {
-		location.reload();
-	});
-	chrome.permissions.onAdded.addListener(() => {
-		location.reload();
-	});
-
-	// Improve textareas editing
-	enableTabToIndent('textarea');
-
-	// Add cache clearer
-	$('#clear-cache').addEventListener('click', clearCacheHandler);
-}
-
-async function init(): Promise<void> {
-	const syncedForm = await generateDom();
-	addEventListeners(syncedForm);
-}
-
-await init();
+// Add cache clearer
+$('#clear-cache').addEventListener('click', clearCacheHandler);
